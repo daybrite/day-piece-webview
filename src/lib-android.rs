28c0aeba388c346b0e43848e8834c208230f49b9
Marc Prud'hommeaux <mwp1@cornell.edu>
2026-07-14 18:08:21 -0400

    Run rustfmt across parts/pieces/tweaks Android sources: sort day_android imports, wrap long lines

--- src/lib-android.rs
+++ src/lib-android.rs
@@ -8,8 +8,8 @@
 // ---------------------------------------------------------------------------
 
 use super::*;
-use day_android::jni::objects::JValue;
 use day_android::DayEnv;
+use day_android::jni::objects::JValue;
 use day_android::{AHandle, Android, with_env};
 use day_spec::NodeId;
 
@@ -29,7 +29,9 @@ fn make(_backend: &mut Android, p: &WebProps, id: NodeId) -> AHandle {
             .expect("DayWebView.makeWebView")
             .l()
             .expect("View");
-        AHandle(std::sync::Arc::new(env.new_global_ref(view).expect("global ref")))
+        AHandle(std::sync::Arc::new(
+            env.new_global_ref(view).expect("global ref"),
+        ))
     })
 }
 
